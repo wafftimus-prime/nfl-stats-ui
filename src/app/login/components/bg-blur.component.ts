114ca66ff3a50bcfd419716466102a7a6fd59299
Giusseppe Theodoredis <giutheo0@gmail.com>
2024-09-18 15:10:50 -0400

    Create bg-blur.component.ts

---
+++ src/app/login/components/bg-blur.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bg-blur',
+  template: `
+    <div
+    class="absolute inset-0 blur-xl max-h-[580px] flex-auto"
+    style="
+      background: linear-gradient(
+        143.6deg,
+        rgba(192, 132, 252, 0) 20.79%,
+        rgba(232, 121, 249, 0.26) 40.92%,
+        rgba(204, 171, 238, 0) 70.35%
+      );
+    "></div>
+  `,
+  standalone: true
+})
+export class BackgroundBlurComponent {
+}
